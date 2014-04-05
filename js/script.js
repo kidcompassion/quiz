0a00deecda902d9435f472cde7e2b5e2fa2d3085
Sally Poulsen <sallypoulsen@gmail.com>
2014-04-05 14:28:21 -0600

    Code is functioning, successfully totalling the number of answers in an array.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,70 @@
 $(document).ready(function(){
 
-//functions
 
-function showQuiz(){
-	$('.next').click(function(e){ //on click of next
-		var active = $('.active');
-		active.addClass('hide').removeClass('active');
-		active.next().removeClass('hide').addClass('active');
-	});
-}
+	//variables
 
-
-function countPoints(){
 	var totalBasic = new Array();
 	var totalPlus = new Array();
 	var totalRV = new Array();
+
+//functions
+
+
+	$('.next').click(function(e){ //on click of next
+
+
+		//submit choice
+		var selectedCat = $('.active input[type=radio]:checked').val();
+		if (selectedCat == 'Basic'){
+			//alert('Basic');
+			totalBasic.push(1);
+		}else if(selectedCat == 'Plus'){
+			//alert('PLUS')
+			totalPlus.push(1);
+		}else if(selectedCat == 'Plus RV'){
+			//alert('PLUS RV')
+			totalRV.push(1);
+		}else {
+			alert('fail');
+		}
+
+
+		var active = $('.active');
+		active.addClass('hide').removeClass('active');
+		active.next().removeClass('hide').addClass('active');
+
+		$('.finished').click(function(e){
+		console.log('basic' + totalBasic.length);
+		console.log('plus' + totalPlus.length);
+		console.log('rv' + totalRV.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
 
 /*	for each question,
 	if (radiobutton has slected basic, add point to total basic array)
@@ -26,26 +76,25 @@ function countPoints(){
 	if it's rv, add a point to rv.
 
 */
-		var selectedBasic = $('input[value="basic"]:radio');
-		var selectedPlus = $('input[value="plus"]:radio');
-		var selectedRV = $('input[value="rv"]:radio');
+
 		
-		if (selectedBasic){
-			alert(selectedBasic);
-		}
-}
+	
 
 
-
-
-countPoints();
+	});
 
 
 
 
 
 
-showQuiz();
+
+
+
+
+
+
+
 
 
 
