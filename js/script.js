10371f8d6cead82fbab6d82e18eb4500a00af6c8
Sally Poulsen <sallypoulsen@gmail.com>
2014-04-06 05:01:57 -0600

    Wires up the previous button so it doesn't skew the results.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
 
-
-	//variables
-
+	//array for number of clicks
 	var totalBasic = new Array();
 	var totalPlus = new Array();
 	var totalRV = new Array();
 
-//functions
 
-	$('.prev').click(function(e){
+	//function to remove a vote on click of previous button
+	$('.prev').click(function(e){ 
+		//shows and hides questions based on click of previous
 		var active = $('.active');
 		active.addClass('hide').removeClass('active');
 		active.prev().removeClass('hide').addClass('active');
 
-
+		//submit choice
+		var selectedCat = $('.active input[type=radio]:checked').val();
+		if (selectedCat == 'Basic'){
+			totalBasic.pop(1);
+		}else if(selectedCat == 'Plus'){
+			totalPlus.pop(1);
+		}else if(selectedCat == 'Plus RV'){
+			totalRV.pop(1);
+		}else {
+			alert('fail');
+		}
 	});
 
-
-	$('.next').click(function(e){ //on click of next
-
-
+	//function to add a vote on click of next
+	$('.next').click(function(e){
 		//submit choice
 		var selectedCat = $('.active input[type=radio]:checked').val();
 		if (selectedCat == 'Basic'){
@@ -36,40 +43,46 @@ $(document).ready(function(){
 			alert('fail');
 		}
 
-
+		//shows and hides each question as you proceed
 		var active = $('.active');
 		active.addClass('hide').removeClass('active');
 		active.next().removeClass('hide').addClass('active');
 
 		$('.finished').click(function(e){
-		
-		
+
+			console.log(totalBasic.length);
+			console.log(totalPlus.length);
+			console.log(totalRV.length);
+				
+					//creates array of objects for each membership level
 					var memberships = [
 				{
 					level: "Basic",
 					price: "$100",
 					totalSelected: totalBasic.length,
-					details: 'Your best option is basic!',
+					details: 'Here are some details about basic',
 
 				},
 				{
 					level: "Plus",
 					price: "$200",
 					totalSelected: totalPlus.length,
-					details: 'your best option is plus!'
+					details: 'Here are some details about plus'
 				},
 				{
 					level: "RV",
 					price: "$300",
 					totalSelected: totalRV.length,
-					details: 'your best option is RV!',
+					details: 'Here are some details about RV',
 				},
 			];
 
+			//sorts objects based on length of arrays
 			memberships.sort(function(a, b) { 
 			    return a.totalSelected - b.totalSelected;
 			});
 
+			
 			for(var i in memberships){
 			    var msg = "<p>Based on your choices, we think your best choice for membership is: </p>"
 			    msg = msg + "<p><strong>" + memberships[i].level + "</strong></p>";
